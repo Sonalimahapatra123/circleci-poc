5b6760304eca88a51fdc5c4a5f4038577cf2d15d
Madhan Ayyasamy <madhan.ayyasamy@idexcel.com>
2019-01-24 23:20:53 +0530

    build no check

--- blog/scripts/deploy.sh
+++ blog/scripts/deploy.sh
@@ -2,8 +2,8 @@
 echo "printing"
 echo $BUILD_NO
 cd ~/circleci-poc && wget https://$BUILD_NO-166982537-gh.circle-artifacts.com/0/root/project/own-artifact/own_artifact.zip && unzip own_artifact.zip
-# source ~/.bash_profile;
-# kill -9 $(lsof -i tcp:3000 -t)
-# echo 'Benchmarking $(pwd)...'
-#cd ~/circleci-poc/usr/src/app && bundle install && rails s -b 0.0.0.0 -d
+source ~/.bash_profile;
+kill -9 $(lsof -i tcp:3000 -t)
+echo 'Benchmarking $(pwd)...'
+cd ~/circleci-poc/usr/src/app && bundle install && rails s -b 0.0.0.0 -d
 
