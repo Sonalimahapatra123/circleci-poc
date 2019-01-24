2ad23259496a72345978e838e4b91b6ef2b6a056
Madhan Ayyasamy <madhan.ayyasamy@idexcel.com>
2019-01-25 00:07:01 +0530

    build no check

--- blog/scripts/deploy.sh
+++ blog/scripts/deploy.sh
@@ -2,7 +2,7 @@
 echo "printing"
 CURRENT_BUILD_NO=$(expr $BUILD_NO - 1)
 echo $CURRENT_BUILD_NO
-cd /home/ubuntu/circleci-poc && wget https://$CURRENT_BUILD_NO-166982537-gh.circle-artifacts.com/0/root/project/own-artifact && unzip own_artifact
+cd /home/ubuntu/circleci-poc && wget https://$CURRENT_BUILD_NO-166982537-gh.circle-artifacts.com/0/root/project/own-artifact && unzip own-artifact
 source ~/.bash_profile;
 kill -9 $(lsof -i tcp:3000 -t)
 echo 'Benchmarking $(pwd)...'
