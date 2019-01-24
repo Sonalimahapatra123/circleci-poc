80a744ca7aa05c0c8272ab2d9d0e97c902b687b0
Madhan Ayyasamy <madhan.ayyasamy@idexcel.com>
2019-01-24 21:46:47 +0530

    deploy ssh check

--- blog/scripts/deploy.sh
+++ blog/scripts/deploy.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 cd ~/circleci-poc && wget https://31-166982537-gh.circle-artifacts.com/0/root/project/own-artifact/own_artifact.zip && unzip own_artifact.zip
-cd /usr/src/app
-
 source ~/.bash_profile;
 kill -9 $(lsof -i tcp:3000 -t)
 echo 'Benchmarking $(pwd)...'
-bundle install && rails s -b 0.0.0.0 -d
+cd ~/circleci-poc/usr/src/app && bundle install && rails s -b 0.0.0.0 -d
 
