d136412f7b3a2acae1a6cafdebce9c62d4b84b97
Madhan Ayyasamy <madhan.ayyasamy@idexcel.com>
2019-01-24 23:36:22 +0530

    build no check

--- blog/scripts/deploy.sh
+++ blog/scripts/deploy.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 echo "printing"
-echo $BUILD_NO
-cd ~/circleci-poc && wget https://$BUILD_NO-166982537-gh.circle-artifacts.com/0/root/project/own-artifact/own_artifact.zip && unzip own_artifact.zip
+CURRENT_BUILD_NO =  expr $BUILD_NO - 1
+echo $CURRENT_BUILD_NO
+cd /home/ubuntu/circleci-poc && wget https://$CURRENT_BUILD_NO-166982537-gh.circle-artifacts.com/0/root/project/own-artifact && unzip own_artifact.zip
 source ~/.bash_profile;
 kill -9 $(lsof -i tcp:3000 -t)
 echo 'Benchmarking $(pwd)...'
-cd ~/circleci-poc/usr/src/app && bundle install && rails s -b 0.0.0.0 -d
+cd /home/ubuntu/circleci-poc/usr/src/app && bundle install && rails s -b 0.0.0.0 -d
 
