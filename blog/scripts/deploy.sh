131b1bb80b81e0713ace69cb4e60e7853f7e8745
Madhan Ayyasamy <madhan.ayyasamy@idexcel.com>
2019-01-24 22:04:50 +0530

    deploy build no check

--- blog/scripts/deploy.sh
+++ blog/scripts/deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-cd ~/circleci-poc && wget https://31-166982537-gh.circle-artifacts.com/0/root/project/own-artifact/own_artifact.zip && unzip own_artifact.zip
+echo $CIRCLE_BUILD_NUM
+cd ~/circleci-poc && rm -rf usr/ && rm own_artifact.zip && wget https://${CIRCLE_BUILD_NUM}-166982537-gh.circle-artifacts.com/0/root/project/own-artifact/own_artifact.zip && unzip own_artifact.zip
 source ~/.bash_profile;
 kill -9 $(lsof -i tcp:3000 -t)
 echo 'Benchmarking $(pwd)...'
