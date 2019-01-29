c0b20e1fe22e7409b1a078ab6246a5b1029056ec
Madhan Ayyasamy <madhan.ayyasamy@idexcel.com>
2019-01-29 17:56:09 +0530

    remove local image in ec2 then compose

--- blog/scripts/deploy.sh
+++ blog/scripts/deploy.sh
@@ -9,6 +9,8 @@ wget https://$CURRENT_BUILD_NO-166982537-gh.circle-artifacts.com/0/root/project/
 # kill -9 $(lsof -i tcp:3000 -t)
 # echo 'Benchmarking $(pwd)...'
 cd /home/ubuntu/circleci-poc/usr/src/app
+sudo docker-compose down
+sudo docker rmi -f madhantry/mdn-images:latest
 sudo docker-compose up -d
 # bundle install && rails s -b 0.0.0.0 -d
 
