74d2c36480fe4e6fe7553feff174b18f2743c0d8
Madhan Ayyasamy <madhan.ayyasamy@idexcel.com>
2019-01-29 21:49:56 +0530

    heading test1

--- blog/scripts/deploy.sh
+++ blog/scripts/deploy.sh
@@ -9,7 +9,7 @@ wget https://$CURRENT_BUILD_NO-166982537-gh.circle-artifacts.com/0/root/project/
 # kill -9 $(lsof -i tcp:3000 -t)
 # echo 'Benchmarking $(pwd)...'
 cd /home/ubuntu/circleci-poc/usr/src/app
-sudo docker-compose down
+sudo docker-compose stop
 sudo docker rmi -f madhantry/mdn-images:latest
 sudo docker pull madhantry/mdn-images:latest && sudo docker-compose up -d
 #sudo docker-compose up -d
