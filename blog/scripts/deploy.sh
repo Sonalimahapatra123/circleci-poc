e3d84646486a184f8a147b150afc80f615fbc8c5
Madhan Ayyasamy <madhan.ayyasamy@idexcel.com>
2019-01-29 16:03:45 +0530

    blog with multiple container test with circle ci deployment

---
+++ blog/scripts/deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# echo "printing"
+# CURRENT_BUILD_NO=$(expr $BUILD_NO - 1)
+# echo $CURRENT_BUILD_NO
+# cd /home/ubuntu/circleci-poc && wget https://$CURRENT_BUILD_NO-166982537-gh.circle-artifacts.com/0/root/project/own-artifact && unzip own-artifact
+# source ~/.bash_profile;
+# kill -9 $(lsof -i tcp:3000 -t)
+# echo 'Benchmarking $(pwd)...'
+# cd /home/ubuntu/circleci-poc/usr/src/app
+# bundle install && rails s -b 0.0.0.0 -d
+
+
