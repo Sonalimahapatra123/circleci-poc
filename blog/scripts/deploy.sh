15e351ac2f462e23c0bd7fb92a3b22359a990281
Madhan Ayyasamy <madhan.ayyasamy@idexcel.com>
2019-01-29 20:18:20 +0530

    post home page title change

--- blog/scripts/deploy.sh
+++ blog/scripts/deploy.sh
@@ -9,11 +9,10 @@ wget https://$CURRENT_BUILD_NO-166982537-gh.circle-artifacts.com/0/root/project/
 # kill -9 $(lsof -i tcp:3000 -t)
 # echo 'Benchmarking $(pwd)...'
 cd /home/ubuntu/circleci-poc/usr/src/app
-sudo docker rmi -f madhantry/mdn-images:latest
-sudo docker pull madhantry/mdn-images:latest
 sudo docker-compose down
-sudo docker-compose run web rake db:create
-sudo docker-compose up -d
+sudo docker rmi -f madhantry/mdn-images:latest
+sudo docker pull madhantry/mdn-images:latest && docker-compose run web rake db:create && docker-compose up -d
+#sudo docker-compose up -d
 # bundle install && rails s -b 0.0.0.0 -d
 
 
