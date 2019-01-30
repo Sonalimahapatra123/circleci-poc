9b75d157b99637d10a0355bd042d730199a23181
Madhan Ayyasamy <madhan.ayyasamy@idexcel.com>
2019-01-30 12:01:06 +0530

    docker-compose rm added

--- blog/scripts/deploy.sh
+++ blog/scripts/deploy.sh
@@ -5,14 +5,10 @@ echo $CURRENT_BUILD_NO
 cd /home/ubuntu/circleci-poc
 rm -rf *
 wget https://$CURRENT_BUILD_NO-166982537-gh.circle-artifacts.com/0/root/project/own-artifact && unzip own-artifact
-# source ~/.bash_profile;
-# kill -9 $(lsof -i tcp:3000 -t)
-# echo 'Benchmarking $(pwd)...'
 cd /home/ubuntu/circleci-poc/usr/src/app
 sudo docker rmi -f madhantry/mdn-images:latest
-sudo docker pull madhantry/mdn-images:latest && sudo docker-compose up -d
-#sudo docker-compose up -d
-# bundle install && rails s -b 0.0.0.0 -d
+sudo docker pull madhantry/mdn-images:latest && sudo docker-compose rm && sudo docker-compose up -d
+
 
 
 
