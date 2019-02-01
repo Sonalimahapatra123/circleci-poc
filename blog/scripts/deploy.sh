2bc89ac2e5f8abf255fc8f007c8735ea079ccf2d
Madhan Ayyasamy <madhan.ayyasamy@idexcel.com>
2019-02-01 12:29:50 +0530

    persistent volume added for mysql

--- blog/scripts/deploy.sh
+++ blog/scripts/deploy.sh
@@ -7,7 +7,7 @@ rm -rf *
 wget https://$CURRENT_BUILD_NO-166982537-gh.circle-artifacts.com/0/root/project/own-artifact && unzip own-artifact
 cd /home/ubuntu/circleci-poc/usr/src/app
 sudo docker rmi -f madhantry/mdn-images:latest
-sudo docker pull madhantry/mdn-images:latest && sudo docker-compose rm && sudo docker-compose up -d
+sudo docker pull madhantry/mdn-images:latest && sudo docker-compose up -d
 
 
 
