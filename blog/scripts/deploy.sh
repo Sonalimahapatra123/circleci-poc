4b29fdaec776572d5237cd20c9dfc96411431b7c
Madhan Ayyasamy <madhan.ayyasamy@idexcel.com>
2019-02-01 17:22:16 +0530

    ec2 stop and start for container data persistancec check

--- blog/scripts/deploy.sh
+++ blog/scripts/deploy.sh
@@ -6,6 +6,7 @@ cd /home/ubuntu/circleci-poc
 rm -rf *
 wget https://$CURRENT_BUILD_NO-166982537-gh.circle-artifacts.com/0/root/project/own-artifact && unzip own-artifact
 cd /home/ubuntu/circleci-poc/usr/src/app
+sudo docker system prune -af
 sudo docker rmi -f madhantry/mdn-images:latest
 sudo docker pull madhantry/mdn-images:latest && sudo docker-compose up -d
 
