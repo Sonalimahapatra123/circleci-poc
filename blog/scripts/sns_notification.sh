4fbfe07db50e51eb5c70822f6650211f6a29326c
Madhan Ayyasamy <madhan.ayyasamy@idexcel.com>
2019-03-19 15:07:26 +0530

    aws sns check

--- blog/scripts/sns_notification.sh
+++ blog/scripts/sns_notification.sh
@@ -3,4 +3,4 @@ apt-get update && apt-get install -y awscli
 aws configure set aws_access_key_id $AWS_ACCESS_KEY_DEV_OPS_OWN_ACCOUNT
 aws configure set aws_secret_access_key $AWS_SECRET_KEY_DEV_OPS_OWN_ACCOUNT
 aws configure set default.region us-east-1
-aws sns publish --topic-arn "arn:aws:sns:us-east-1:546124439885:circleci_each_job_status" --message "CIRCLECI JOB STATUS: ${CIRCLE_JOB} $1" --subject "CIRCLECI JOB STATUS: ${CIRCLE_JOB} $1"
+aws sns publish --topic-arn "arn:aws:sns:us-east-1:546124439885:circleci_each_job_status" --message echo ("CIRCLECI JOB STATUS: JOB NAME IS ${CIRCLE_JOB} $1" | tr [a-z] [A-Z]) --subject " JOB NAME IS ${CIRCLE_JOB} $1"
