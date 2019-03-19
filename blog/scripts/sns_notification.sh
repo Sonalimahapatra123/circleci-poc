742952c63ece33b4c90cf832a5b8762420750b52
Madhan Ayyasamy <madhan.ayyasamy@idexcel.com>
2019-03-19 19:11:38 +0530

    build status email start/success/failure sns notification added

--- blog/scripts/sns_notification.sh
+++ blog/scripts/sns_notification.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
-apt-get update && apt-get install -y awscli
+if ["$CIRCLE_JOB" == "BUILD" ]; then
+  apk update && apk -v --update add \
+                            python \
+                            py-pip \
+                            groff \
+                            less \
+                            mailcap \
+                            && \
+                            pip install --upgrade awscli==1.14.5 s3cmd==2.0.1 python-magic && \
+                            apk -v --purge del py-pip && \
+                            rm /var/cache/apk/*
+else                            
+  apt-get update && apt-get install -y awscli
+fi
+
 aws configure set aws_access_key_id $AWS_ACCESS_KEY_DEV_OPS_OWN_ACCOUNT
 aws configure set aws_secret_access_key $AWS_SECRET_KEY_DEV_OPS_OWN_ACCOUNT
 aws configure set default.region us-east-1
