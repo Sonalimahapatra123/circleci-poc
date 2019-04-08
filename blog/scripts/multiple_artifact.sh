d9e4875425261bec5945ad84c218780652951757
Madhan Ayyasamy <madhan.ayyasamy@idexcel.com>
2019-04-08 18:12:21 +0530

    compress artifact line added in buildspec

--- blog/scripts/multiple_artifact.sh
+++ blog/scripts/multiple_artifact.sh
@@ -1,40 +1,37 @@
 #!/bin/bash
 shopt -s extglob
 set -e
-
 # rm -rf circleci-poc-unicorn && rm -rf circleci-poc-sidekiq && rm -rf circleci-poc-cron
-# cp -vr $CODEBUILD_SRC_DIR/blog circleci-poc-unicorn
+cp -r $CODEBUILD_SRC_DIR/blog circleci-poc-unicorn
 cp -r $CODEBUILD_SRC_DIR/blog circleci-poc-sidekiq
-# cp -vr $CODEBUILD_SRC_DIR/blog circleci-poc-cron
+cp -r $CODEBUILD_SRC_DIR/blog circleci-poc-cron
 
 # For unicorn
-# cd circleci-poc-unicorn && rm -rf Gemfile Gemfile.lock vendor/* gemfile_for_sidekiq gemfile_for_cron
-# mv gemfile_for_unicorn Gemfile
-# bundle package && bundle install --local
+cd circleci-poc-unicorn && rm -rf Gemfile Gemfile.lock vendor/* gemfile_for_sidekiq gemfile_for_cron
+mv gemfile_for_unicorn Gemfile
+bundle package && bundle install --local
 
 
-#For sidekiq related files
+#For sidekiq related files 
 cd circleci-poc-sidekiq && rm -rf Gemfile Gemfile.lock vendor/* gemfile_for_unicorn gemfile_for_cron
 mv gemfile_for_sidekiq Gemfile
-echo "going to remove unwanted files" 
 cd app && rm -rf !(workers|models)
-echo "unwanted files removal done"
 cd ../
 rm -rf !(bin|app|Gemfile|config|db|log|scripts|vendor)
 bundle package && bundle install --local #&& bundle exec sidekiq -d -L log/sidekiq.log
 
 
 #For cron
-# cd circleci-poc-cron && rm -rf Gemfile Gemfile.lock vendor/* gemfile_for_unicorn gemfile_for_sidekiq
-# mv gemfile_for_cron Gemfile
+cd circleci-poc-cron && rm -rf Gemfile Gemfile.lock vendor/* gemfile_for_unicorn gemfile_for_sidekiq
+mv gemfile_for_cron Gemfile
 
-# # sidekiq related files to delete
-# cd app && rm -rf !(models)
-# cd ../
-# echo $(pwd)
-# rm -r config/initializers
-# cd config && rm -rf !(environments|application.rb|boot.rb|schedule.rb|database.yml|environment.rb)
-# cd ../
-# echo $(ls)
-# rm -rf !(bin|app|Gemfile|config|db|log|scripts|vendor|lib|Rakefile)
-# bundle package && bundle install --local #&& whenever --update-crontab --set environment=development
+# sidekiq related files to delete
+cd app && rm -rf !(models)
+cd ../
+echo $(pwd)
+rm -r config/initializers
+cd config && rm -rf !(environments|application.rb|boot.rb|schedule.rb|database.yml|environment.rb)
+cd ../
+echo $(ls)
+rm -rf !(bin|app|Gemfile|config|db|log|scripts|vendor|lib|Rakefile)
+bundle package && bundle install --local #&& whenever --update-crontab --set environment=development
