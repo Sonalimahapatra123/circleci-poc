63cec89ea3a8ad62a78592eb9ed50fb229f90f99
Madhan Ayyasamy <madhan.ayyasamy@idexcel.com>
2019-04-08 19:11:16 +0530

    shell script file split for each type

--- blog/scripts/unicorn_artifact.sh
+++ blog/scripts/unicorn_artifact.sh
@@ -8,14 +8,3 @@ cp -r $CODEBUILD_SRC_DIR/blog circleci-poc-unicorn
 cd circleci-poc-unicorn && rm -rf Gemfile Gemfile.lock vendor/* gemfile_for_sidekiq gemfile_for_cron
 mv gemfile_for_unicorn Gemfile
 bundle package && bundle install --local
-
-cd ../
-#For sidekiq related files 
-cd circleci-poc-sidekiq && rm -rf Gemfile Gemfile.lock vendor/* gemfile_for_unicorn gemfile_for_cron
-mv gemfile_for_sidekiq Gemfile
-cd app && rm -rf !(workers|models)
-cd ../
-rm -rf !(bin|app|Gemfile|config|db|log|scripts|vendor)
-echo "gem file content "
-echo $(cat Gemfile)
-bundle package && bundle install --local #&& bundle exec sidekiq -d -L log/sidekiq.log
