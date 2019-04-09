8fe8a9628d1c34341e8758c362821087af9a3e50
Madhan Ayyasamy <madhan.ayyasamy@idexcel.com>
2019-04-09 15:46:29 +0530

    gem file wihtout local

--- blog/scripts/cron_artifact.sh
+++ blog/scripts/cron_artifact.sh
@@ -11,20 +11,11 @@ mv gemfile_for_cron Gemfile
 # sidekiq related files to delete
 cd app && rm -rf !(models)
 cd ../
-echo $(pwd)
 rm -r config/initializers
 cd config && rm -rf !(environments|application.rb|boot.rb|schedule.rb|database.yml|environment.rb)
 cd ../
-echo $(ls)
 rm -rf !(bin|app|Gemfile|config|db|log|scripts|vendor|lib|Rakefile)
-echo "gem file content "
-echo "current direc tory"
-echo $(pwd)
+echo "cron gem file"
 echo $(cat Gemfile)
-echo $(pwd)
-echo "form current"
-echo $(ls)
-echo $(cat Gemfile)
-
 bundle package && bundle install --gemfile Gemfile #&& whenever --update-crontab --set environment=development
 exit 0
